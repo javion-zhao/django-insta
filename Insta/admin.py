cdd69e2cd09265a4e4da4539607051f3597ee4df
jiawei-zhao <zhaojiawei5731@gmail.com>
2020-03-11 03:39:49 -0400

    instagram

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from Insta.models import  Post
+from Insta.models import  Post,InstaUser,Like,Comment
 
 
 admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(InstaUser)
--- Insta/urls.py
+++ Insta/urls.py
@@ -1,4 +1,4 @@
-"""instaJZ URL Configuration
+"""instagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,8 +16,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostListView, PostDetailView, PostCreatView, PostUpdateView,PostDeleteView,addLike,UserDetailView,addComment,toggleFollow,EditProfile
 
 urlpatterns = [
-    path('',HelloWorld.as_view(),name='HelloWorld'),
+    path('index', HelloWorld.as_view(), name='HelloWorld'),
+    path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('new/', PostCreatView.as_view(), name='make_post'),
+    path('post/edit/<int:pk>/',PostUpdateView.as_view(),name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('like', addLike , name='addlike'),
+    path('user/<int:pk>/',UserDetailView.as_view(),name='user_detail'),
+    path('comment/<int:pk>/',addComment,name='addcomment'),
+    path('togglefollow', toggleFollow, name='togglefollow'),
+    path('edit_user/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
 ]
